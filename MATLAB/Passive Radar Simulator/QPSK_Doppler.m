7a6c944f9430a851d1e1e147db15fff628a4a405
MiguelStattmiller <80427307+MiguelStattmiller@users.noreply.github.com>
2022-04-14 17:56:13 +0100

    Add files via upload

--- MATLAB/Passive Radar Simulator/QPSK_Doppler.m
+++ MATLAB/Passive Radar Simulator/QPSK_Doppler.m
@@ -9,7 +9,8 @@
 % Miguel Albuquerque, Escola Naval, 2022
 
 
-% The current program is a passive radar simulator for static targets with zero-doppler values, using QPSK transmitted signal.
+% The current program is a simulator for testing delay-doppler with noise parameters for moving targets
+% using QPSK transmitted signal.
 % QPSK signal is created based on a message written in .txt file
 % For the passive radar: Reference signal= QPSK signal
 %                        Surveillance signal=QPSK_signal delayed in Time 
@@ -24,8 +25,8 @@ clc
 %***************** INPUTS ******************
 fc=30e6; %Carrier frequency
 c=3e8; % Speed of light 
-lambda=c/fc; %Wavelength
 Rb=fc/100; %Bitrate
+%SNR=10; %SNR [dB]
 fontsize=12;
 SentenceFilename = 'Mensagem_short.txt';
 MeasuredFilename = 'ImplantableAntenna2Reader.s2p';
@@ -112,120 +113,79 @@ grid on;
  grid on;
  hold off
 
-%**************** Define targets, Surveillance area and radar positions
 
-% Define surveillance area and targets
-Lp=1; % Pixel length
-Nx= zeros(1,200); % dimension in x, horizontal of surveillance area
-Ny= zeros(200,1); % dimension in y, vertical  of surveillance area
+%**************** Target description
 
-
-AoI=Nx.*Ny; % Surveillance area
-AoI(4,(3:7)) = 1; % define target, set row 4, from column 3-7 to 1
+v=300.0; %Target speed
+lambda=c/fc; %Wavelength
+dopplershift=speed2dop(v,lambda); %Convert speed to doppler shift 
 
 
-% Receiver antenna position
-X_receiver=100*Lp;
-Y_receiver=97*Lp;
-
-% Transmitter antenna position
-
-X_transmitter=100*Lp;
-Y_transmitter=7*Lp;
+f=freq+dopplershift;
+Signal_dopplershift=ifft(f);
 
 
+%**************** Add White noise to Surveillance_Signal
 
-% Search for targets in surveillance area
-
-for xx=1:200
-    for yy=1:200
-         if AoI(xx,yy) ~= 0 % Target detection
-            X_target= xx*Lp;
-            Y_target= yy*Lp;
-            angle_transmitter =atan2(X_transmitter-X_target,Y_transmitter-Y_target);
-            angle_transmitter = rad2deg(angle_transmitter);
-            angle_receiver =atan2(X_receiver-X_target,Y_receiver-Y_target);
-            angle_receiver = rad2deg(angle_receiver);
-             
-            status=snell_function(angle_transmitter,angle_receiver);
-            P=LoS(AoI,X_transmitter,Y_transmitter,Lp);
-
-             if status==1 && P==-1
-
-                 R1=sqrt( (X_transmitter-X_target).^2 + (Y_transmitter-Y_target).^2); % Distance transmitter-target
-                 R2=sqrt( (X_receiver-X_target).^2 + (Y_receiver-Y_target).^2); % Distance Receiver-target
-                 L=sqrt( (X_receiver-X_transmitter).^2 + (Y_receiver-Y_transmitter).^2); % Distance Transmitter-Receiver
-
-             end
-        end
-    end
-end
-
-k0=(2*pi*fc)/c;
-X_QPSK=fft(Reference_Signal);
-Reference_Signal=(1/L)*X_QPSK*exp(-1*j*k0*L);
-Surveillance_Signal=(1/(R1+R2))*X_QPSK*exp(-1*j*k0*(R1+R2));
-Reference_Signal=ifft(Reference_Signal);
-Surveillance_Signal=ifft(Surveillance_Signal);
-
+SNR=50;
+Surveillance_Signal=awgn(Signal_dopplershift,SNR,'measured');
 
 %**************** Calculate ambiguity and cross-ambiguity functions 
 
 %Reference_Signal ambiguity function
-[afmag,delay] = ambgfun(Reference_Signal,fs,1e6,'Cut','Doppler');
+[afmag,doppler] = ambgfun(Reference_Signal,fs,1e6,'Cut','Delay');
 afmag = afmag*1; % Select plot gain *1
 afmag(afmag>1 )= 1;
 
  %Surveillance_Signal ambiguity function
-[afmag2,delay2] = ambgfun(Surveillance_Signal,fs,1e6,'Cut','Doppler');
+[afmag2,doppler2] = ambgfun(Surveillance_Signal,fs,1e6,'Cut','Delay');
 afmag2 = afmag2*1; % Select plot gain *1
 afmag2(afmag2>1 )= 1;
 
 %Cross-ambiguity
-[afmag3,delay3] = ambgfun(Reference_Signal,Surveillance_Signal,fs,[1e6 1e6],'Cut','Doppler');
+[afmag3,doppler3] = ambgfun(Reference_Signal,Surveillance_Signal,fs,[1e6 1e6],'Cut','Delay');
 afmag3 = afmag3*1; % Select plot gain *1
 afmag3(afmag3>1 )= 1;
-
 
 %**************** Plot ambiguity and cross-ambiguity functions
 
 
 %Plot Ambiguity Function of Sref
 [pks1, index] = max(afmag);
-xMax = delay(index);
+xMax = doppler(index);
 yMax = pks1;
 subplot(3,2,1)
-plot(delay,afmag,'LineStyle','-.'); 
+plot(doppler,afmag,'LineStyle','-.'); 
 hold on
-plot3(delay(pks1,index),afmag(pks1,index), pks1, '^r')
+%plot3(doppler(pks1,index),afmag(pks1,index), pks1, '^r')
 textString = sprintf('(%f, %f)', xMax, yMax);
 text(xMax, yMax,textString,"Color",'b','FontSize',10);
 hold off
 shading interp;
-xlim ([-8e-5 8e-5]);
+xlim auto;
 grid on; 
 colorbar;
-xlabel('Delay \tau (s)');
+xlabel('Doppler (Hz)');
 ylabel('Ambiguity Function Magnitude');
 title('Ambiguity Function Sref');
 
 
 %Plot Ambiguity Function of Sr
 [pks2,index2] = max(afmag2);
-xMax2 = delay2(index2);
+xMax2 = doppler2(index2);
 yMax2 = pks2;
 subplot(3,2,2)   
-plot(delay2,afmag2,'LineStyle','-'); 
+plot(doppler2,afmag2,'LineStyle','-'); 
 hold on
-plot3(delay2(pks2,index2), afmag2(pks2,index2), pks2, '^r')
+%plot3(doppler2(pks2,index2), afmag2(pks2,index2), pks2, '^r')
 textString2 = sprintf('(%f, %f)', xMax2, yMax2);
 text(xMax2, yMax2,textString2,"Color",'b','FontSize',10);
 hold off
 shading interp;
-xlim([-8e-5 8e-5]);
+xlim auto;
 grid on; 
 colorbar;
-xlabel('Delay \tau (us)');
+xlabel('Doppler (Hz)');
 ylabel('Ambiguity Function Magnitude');
 title('Ambiguity Function Sr');
 
@@ -233,20 +193,19 @@ title('Ambiguity Function Sr');
 % Plot cross-ambiguity function of Sref and Sr
 
 [pks3,index3] = max(afmag3);
-xMax3 = delay3(index3);
+xMax3 = doppler3(index3);
 yMax3 = pks3;
 subplot(3,2,3)
-plot(delay3,afmag3,'LineStyle','-'); 
+plot(doppler3,afmag3,'LineStyle','-'); 
 hold on
-%plot3(delay3(pks3,index3), afmag3(pks3,index3), pks3, '^r')
 textString3 = sprintf('(%f, %f)', xMax3, yMax3);
 text(xMax3, yMax3,textString3,"Color",'b','FontSize',10);
 hold off
 shading interp;
-xlim([-8e-5 8e-5]);
+xlim auto;
 grid on; 
 colorbar;
-xlabel('Delay \tau (s)');
+xlabel('Doppler (Hz)');
 ylabel('Ambiguity Function Magnitude');
 title('Cross-ambiguity');
 
@@ -254,16 +213,25 @@ title('Cross-ambiguity');
 % Plot ambiguity function of Sref, Sr and cross-ambiguity
 
 subplot(3,2,4)
-plot(delay,afmag,'LineStyle','-.','Color','b'); % Green Sref
+plot(doppler,afmag,'LineStyle','-.','Color','b'); % Green Sref
 hold on
-plot(delay2, afmag2,'LineStyle','-','Color','r'); % Red Sr
-plot(delay3, afmag3,'LineStyle','--','Color','g'); % blue cross-ambiguity 
+plot(doppler2, afmag2,'LineStyle','-','Color','r'); % Red Sr
+plot(doppler3, afmag3,'LineStyle','--','Color','g'); % blue cross-ambiguity 
 hold off
-xlim ([-4e-5 4e-5]);
+xlim auto;
 grid on; 
 colorbar;
-xlabel('Delay \tau (s)');
+xlabel('Doppler (Hz)');
 ylabel('Ambiguity Function Magnitude');
 title('Sref, Sr and cross-ambiguity');
 legend('Sref','Sr','cross-ambiguity');
+
+
+
+
+
+
+
+
+
 
