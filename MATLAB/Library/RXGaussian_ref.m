0b97b5eafe4f7c556ab92be4533909acfd6baa90
MiguelStattmiller <80427307+MiguelStattmiller@users.noreply.github.com>
2022-05-10 21:37:13 +0100

    Add files via upload

--- MATLAB/Library/RXGaussian_ref.m
+++ MATLAB/Library/RXGaussian_ref.m
@@ -1,29 +1,21 @@
-function [Wi_ref] = RXGaussian_ref(X_receiverref,Y_receiverref,X_transmitter,Y_transmitter,alpha_zeroSVref,D_ref)
+function [Wi_ref] = RXGaussian_ref(X_receiverref,Y_receiverref,X_transmitter,Y_transmitter,alpha_zeroSVref,theta_3dB_ref)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-
-%**************** INPUTS
-
-alpha_zeroSVref=2; % Introduce desired receiver antenna angle in radians
-D_ref=1;% Antenna length in meters
-n=1; % Antenna directivity
-
-
 %**************** Function Calculus
-theta_3dB=sqrt(4*pi/D_ref);
+n=real((1/2*log10(2))/(log10(cos(theta_3dB_ref))));
 alpha=linspace(alpha_zeroSVref-pi/2,pi/2+alpha_zeroSVref);
 
 % Define vector between receiver and transmitter
 V=[X_receiverref-X_transmitter Y_receiverref-Y_transmitter];
-angle = atan2(abs(V(2)), V(1));
+alpha_DoA = atan2(abs(V(2)), V(1));
 
 
 %u = [cos(alpha_zeroSVref),sin(alpha_zeroSVref)]; % direction beam of receiver antenna
 %angle2 = atan2(abs(u(2)), u(1));
 
-if  and(angle>alpha(1), angle<alpha(end))
-    Wi_ref=cos(angle-alpha_zeroSVref)^n;
+if  and(alpha_DoA>alpha(1), alpha_DoA<alpha(end))
+    Wi_ref=cos(alpha_DoA-alpha_zeroSVref)^n;
 else
      Wi_ref= 0;
 end
