d5b800777dc8174d2a80403f961d37c3fbd046a2
MiguelStattmiller <80427307+MiguelStattmiller@users.noreply.github.com>
2022-05-10 16:29:53 +0100

    Add files via upload

--- MATLAB/Library/RXGaussian.m
+++ MATLAB/Library/RXGaussian.m
@@ -5,31 +5,28 @@ function [Wi_Surv] = RXGaussian(X_receiver,Y_receiver,X_target,Y_target,alpha_ze
 
 %**************** INPUTS
 
-alpha_zeroSV=2 % Introduce desired receiver antenna angle in radians
-D_surv=1 % Antenna length in meters
-n=1; % Antenna directivity
+alpha_zero_surv=3*pi/4; % Introduce desired receiver antenna angle in radians
+D_surv=1; % Antenna length in meters
+n=3;
+
 
 %**************** Function Calculus
+theta_3dB=sqrt(4*pi/D_surv);
+alpha=linspace(alpha_zero_surv-pi/2,pi/2+alpha_zero_surv);
 
 % Define vector between receiver and target
+
 V=[X_receiver-X_target Y_receiver-Y_target];
-angle = atan2(abs(V(2)), V(1));
+alpha_DoA = atan2(abs(V(2)), V(1));
 
-u = [cos(alpha_zeroSV),sin(alpha_zeroSV)]; % direction beam of receiver antenna
-angle2 = atan2(abs(u(2)), u(1));
+%u = [cos(alpha_zero_surv),sin(alpha_zero_surv)]; % direction beam of receiver antenna
+%alpha_zero = atan2(abs(u(2)), u(1));
 
-
-
-Wi_Surv= cos(angle-angle2)^n;
-
-
-
-
-
-
-
-
-
+if  and(alpha_DoA>alpha(1), alpha_DoA<alpha(end))
+    Wi_Surv= cos(alpha_DoA-alpha_zero_surv)^n;
+else
+     Wi_Surv= 0;
+end
 
 
 
--- MATLAB/Library/RXGaussian_ref.m
+++ MATLAB/Library/RXGaussian_ref.m
@@ -5,22 +5,27 @@ function [Wi_ref] = RXGaussian_ref(X_receiverref,Y_receiverref,X_transmitter,Y_t
 
 %**************** INPUTS
 
-alpha_zeroSVref=2 % Introduce desired receiver antenna angle in radians
-D_ref=1 % Antenna length in meters
+alpha_zeroSVref=2; % Introduce desired receiver antenna angle in radians
+D_ref=1;% Antenna length in meters
 n=1; % Antenna directivity
 
 
 %**************** Function Calculus
+theta_3dB=sqrt(4*pi/D_ref);
+alpha=linspace(alpha_zeroSVref-pi/2,pi/2+alpha_zeroSVref);
 
 % Define vector between receiver and transmitter
 V=[X_receiverref-X_transmitter Y_receiverref-Y_transmitter];
 angle = atan2(abs(V(2)), V(1));
 
 
-u = [cos(alpha_zeroSVref),sin(alpha_zeroSVref)]; % direction beam of receiver antenna
-angle2 = atan2(abs(u(2)), u(1));
+%u = [cos(alpha_zeroSVref),sin(alpha_zeroSVref)]; % direction beam of receiver antenna
+%angle2 = atan2(abs(u(2)), u(1));
+
+if  and(angle>alpha(1), angle<alpha(end))
+    Wi_ref=cos(angle-alpha_zeroSVref)^n;
+else
+     Wi_ref= 0;
+end
 
 
-
-
-Wi_ref=cos(angle-angle2)^n;
