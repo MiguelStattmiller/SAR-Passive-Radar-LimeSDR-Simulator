66e57c6faea0ff5be13f4ec83804ea322d978cf6
MiguelStattmiller <80427307+MiguelStattmiller@users.noreply.github.com>
2022-05-25 19:14:49 +0100

    Add files via upload

--- MATLAB/Library/snell_function.m
+++ MATLAB/Library/snell_function.m
@@ -1,14 +1,15 @@
-function [status] = snell_function(angle_transmitter,angle_receiver,teta_flutuations)
+function [status] = snell_function(angle_transmitter,angle_receiver,teta_flutuations,Vectors_product_2)
 % This function presents the verification of snell law,for the angle_transmitter (between
 % Transmitter and target) and the angle_receiver(between target and
 % receiver)
 % if verification of this law occurs , status=1 else status=0.
 
-if angle_transmitter <= angle_receiver + teta_flutuations && angle_transmitter >= angle_receiver - teta_flutuations;
+if angle_transmitter <= angle_receiver + teta_flutuations && angle_transmitter >= angle_receiver - teta_flutuations &&  Vectors_product_2 ~= 1
 
     status=1;
 
 else
+
     status=0;
 
 end
