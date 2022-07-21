981ece75474642f846eee4d198a73ceecc99dcf2
MiguelStattmiller <80427307+MiguelStattmiller@users.noreply.github.com>
2022-07-21 22:09:11 +0100

    Add files via upload

---
+++ MATLAB/Passive Radar Simulator/plots.m
@@ -0,0 +1,437 @@
+%***************** Spectrum formation of both signals ******************
+figure;
+ic=find(~all(surv_matrix==0));
+hL=plot(abs(doppler_freqSurv),20*log10(abs(surv_matrix(:,ic))));
+hLg=legend('1 coluna do avião','2 coluna do avião','3 coluna do avião','4 coluna do avião','5 coluna do avião','6 coluna do avião','7 coluna do avião');
+xlabel('Frequency (Hz)');
+ylabel('Spectrum (dB)');
+title('Surveillance Spectrum');
+
+
+figure;
+ic=find(~all(ref_matrix==0));
+hL=plot(abs(doppler_freqRef),20*log10(abs(ref_matrix(:,ic))));
+hLg=legend('1 coluna do avião','2 coluna do avião','3 coluna do avião','4 coluna do avião','5 coluna do avião','6 coluna do avião','7 coluna do avião');
+xlabel('Frequency (Hz)');
+ylabel('Spectrum (dB)');
+title('Reference Spectrum');
+%***************** Data Visualization of both signals  ******************
+
+figure;
+contour(waypoints,freq_XQPSK_cut,20*log10(abs(surv_matrix)));
+colorbar;
+xlabel('Waypoints (m)');
+ylabel('Frequency (Hz)');
+ylim([2.7e7 3.3e7]);
+title('Surveillance matrix contour');
+
+figure;
+contour(1:400,freq_XQPSK_cut,20*log10(abs(ref_matrix)));
+colorbar;
+xlabel('Waypoints (m)');
+ylabel('frequency (Hz)');
+ylim([2.7e7 3.3e7]);
+title('Reference matrix contour');
+
+figure;
+imagesc(1:400,freq_XQPSK_cut,20*log10(abs(surv_matrix)));
+title('Surveillance Signal Raw Data');
+colorbar;
+xlabel('Waypoints (m)');
+ylabel('Frequency Signal (Hz)');
+ylim([2.7e7 3.3e7]);
+xlim([0 400]);
+
+figure;
+imagesc(1:400,freq_XQPSK_cut,20*log10(abs(ref_matrix)));
+title('Reference Signal Raw Data');
+colorbar;
+xlabel('Waypoints (m)');
+ylabel('Frequency Signal (Hz)');
+ylim([2.7e7 3.3e7]);
+xlim([0 400]);
+
+%**************** Plot Reference and Surveillance Signals Time/ Frequency Domain 
+
+% Plot signals in Time Domain
+plot(time_RS,abs(Reference_Signal));
+hold on
+plot(time_SS,abs(Surveillance_Signal));
+legend('Reference Signal','Surveillance Signal');
+xlabel('Time (s)');
+ylabel('Spectrum');
+title('Time domain');
+xlim([-2e-4 -1.5e-4]);
+
+
+% Frequency
+
+plot(abs(doppler_freqSurv),abs(Surveillance_SignalFD));
+hold on;
+plot(abs(doppler_freqRef),abs(Reference_SignalFD));
+plot(abs(freq_XQPSK_cut),abs(X_QPSK_cut));
+legend('Surveillance Signal','Reference Signal','QPSK');
+xlabel('freq (Hz)');
+ylabel('Spectrum');
+title('Frequency domain');
+ylim([0 1.1]);
+
+%***************** Plot the interpolation data  ******************
+
+
+% Time Domain
+plot(RSInterp_time,abs(Reference_interp));
+hold on
+plot(SSInterp_time,abs(Surveillance_interp));
+legend('Reference Signal','Surveillance Signal');
+xlabel('Time (s)');
+ylabel('Spectrum');
+title('Interp Time domain');
+
+%**************** Plot ambiguity and cross-ambiguity functions time delay
+
+
+%Plot Ambiguity Function of Sref
+[pks1, index] = max(afmag);
+xMax = delay(index);
+yMax = pks1;
+subplot(3,2,1)
+plot(delay,afmag,'LineStyle','-.'); 
+hold on
+textString = sprintf('(%f, %f)', xMax, yMax);
+text(xMax, yMax,textString,"Color",'b','FontSize',10);
+hold off
+shading interp;
+xlim auto;
+grid on; 
+colorbar;
+xlabel('Delay \tau (s)');
+ylabel('Ambiguity Function Magnitude');
+title('Ambiguity Function Sref');
+
+
+%Plot Ambiguity Function of Sr
+[pks2,index2] = max(afmag2);
+xMax2 = delay2(index2);
+yMax2 = pks2;
+subplot(3,2,2)   
+plot(delay2,afmag2,'LineStyle','-'); 
+hold on
+textString2 = sprintf('(%f, %f)', xMax2, yMax2);
+text(xMax2, yMax2,textString2,"Color",'b','FontSize',10);
+hold off
+shading interp;
+xlim auto;
+grid on; 
+colorbar;
+xlabel('Delay \tau (us)');
+ylabel('Ambiguity Function Magnitude');
+title('Ambiguity Function Sr');
+
+
+% Plot cross-ambiguity function of Sref and Sr
+
+[pks3,index3] = max(afmag3);
+xMax3 =delay3(index3);
+yMax3 = pks3;
+subplot(3,2,3)
+plot(delay3,afmag3,'LineStyle','-'); 
+hold on
+textString3 = sprintf('(%.2e,%f)',xMax3, yMax3);
+text(xMax3, yMax3,textString3,"Color",'b','FontSize',10);
+hold off
+shading interp;
+xlim auto;
+grid on; 
+colorbar;
+xlabel('delay (s)');
+ylabel('Ambiguity Function Magnitude');
+title('Cross-ambiguity');
+
+
+% Plot ambiguity function of Sref, Sr and cross-ambiguity
+
+subplot(3,2,4)
+plot(delay,afmag,'LineStyle','-.','Color','b'); % Green Sref
+hold on
+plot(delay2, afmag2,'LineStyle','-','Color','r'); % Red Sr
+plot(delay3, afmag3,'LineStyle','--','Color','g'); % blue cross-ambiguity 
+hold off
+xlim auto;
+grid on; 
+colorbar;
+xlabel('Delay \tau (s)');
+ylabel('Ambiguity Function Magnitude');
+title('Sref, Sr and cross-ambiguity');
+legend('Sref','Sr','cross-ambiguity');
+
+
+
+%**************** Plot ambiguity and cross-ambiguity functions doppler delay
+
+%Plot Ambiguity Function of Sref
+[pks1, index] = max(afmag);
+xMax = doppler(index);
+yMax = pks1;
+subplot(3,2,1)
+plot(doppler,afmag,'LineStyle','-.'); 
+hold on
+%plot3(doppler(pks1,index),afmag(pks1,index), pks1, '^r')
+textString = sprintf('(%f, %f)', xMax, yMax);
+text(xMax, yMax,textString,"Color",'b','FontSize',10);
+hold off
+shading interp;
+xlim auto;
+grid on; 
+colorbar;
+xlabel('Doppler (Hz)');
+ylabel('Ambiguity Function Magnitude');
+title('Ambiguity Function Sref');
+
+
+%Plot Ambiguity Function of Sr
+[pks2,index2] = max(afmag2);
+xMax2 = doppler2(index2);
+yMax2 = pks2;
+subplot(3,2,2)   
+plot(doppler2,afmag2,'LineStyle','-'); 
+hold on
+%plot3(doppler2(pks2,index2), afmag2(pks2,index2), pks2, '^r')
+textString2 = sprintf('(%f, %f)', xMax2, yMax2);
+text(xMax2, yMax2,textString2,"Color",'b','FontSize',10);
+hold off
+shading interp;
+xlim auto;
+grid on; 
+colorbar;
+xlabel('Doppler (Hz)');
+ylabel('Ambiguity Function Magnitude');
+title('Ambiguity Function Sr');
+
+
+% Plot cross-ambiguity function of Sref and Sr
+
+[pks3,index3] = max(afmag3);
+xMax3 = doppler3(index3);
+yMax3 = pks3;
+subplot(3,2,3)
+plot(doppler3,afmag3,'LineStyle','-'); 
+hold on
+textString3 = sprintf('(%.2e, %f)', xMax3, yMax3);
+text(xMax3, yMax3,textString3,"Color",'b','FontSize',10);
+hold off
+shading interp;
+grid on; 
+colorbar;
+xlabel('Doppler (Hz)');
+ylabel('Ambiguity Function Magnitude');
+title('Cross-ambiguity');
+
+
+% Plot ambiguity function of Sref, Sr and cross-ambiguity
+
+subplot(3,2,4)
+plot(doppler,afmag,'LineStyle','-.','Color','b'); % Green Sref
+hold on
+plot(doppler2, afmag2,'LineStyle','-','Color','r'); % Red Sr
+plot(doppler3, afmag3,'LineStyle','--','Color','g'); % blue cross-ambiguity 
+hold off
+xlim ([0.5e6 1.2e6]);
+grid on; 
+colorbar;
+xlabel('Doppler (Hz)');
+ylabel('Ambiguity Function Magnitude');
+title('Sref, Sr and cross-ambiguity');
+legend('Sref','Sr','cross-ambiguity');
+
+%**************** Plot ambiguity and cross-ambiguity functions time and doppler delay
+
+%Plot Ambiguity Function of Sref
+subplot(3,2,1)
+surf(delay,doppler,afmag,'LineStyle','-.');
+shading interp;
+%axis([-0.5e-5 0.5e-5 -10 10]); 
+grid on; 
+view([140,35]); 
+colorbar;
+xlabel('delay (s)');
+ylabel('doppler (Hz)');
+zlabel('Ambiguity Function Magnitude');
+title('Ambiguity Function Sref');
+
+
+%Plot Ambiguity Function of Sr
+subplot(3,2,2)
+surf(delay2,doppler2,afmag2,'LineStyle','-.');
+shading interp;
+%axis([-0.5e-5 0.5e-5 -10000 10000]); 
+grid on; 
+view([140,35]); 
+colorbar;
+xlabel('delay (s)');
+ylabel('doppler (Hz)');
+zlabel('Ambiguity Function Magnitude');
+title('Ambiguity Function Sr');
+
+
+% Plot cross-ambiguity function of Sref and Sr
+
+figure;
+surf(delay3,doppler3,afmag3,'LineStyle','-.');
+shading interp;
+%axis([-0.5e-5 0.5e-5 -4e6 4e6]); 
+grid on; 
+view([140,35]); 
+colorbar;
+xlabel('delay (s)');
+ylabel('doppler (Hz)');
+zlabel('Ambiguity Function Magnitude');
+title('Cross-ambiguity Function');
+
+
+%***************** Range compressed data representation  ******************
+ 
+figure;
+maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
+contourf(1:400,range,20*log10(abs(range_compressed_matrix)),'edgecolor','none');
+%contourf(20*log10(abs(range_compressed_matrix)),'edgecolor','none');
+colorbar;
+colormap(jet);
+caxis([maxcolorbar-30 maxcolorbar]);
+title('Range Compressed Data');
+xlabel('Waypoints (m)');
+ylabel('Range (m)');
+
+subplot(3,2,1);
+%maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
+contourf(1:400,range,abs(range_compressed_matrix),'edgecolor','none');
+%contourf(20*log10(abs(range_compressed_matrix)),'edgecolor','none');
+colorbar;
+colormap(jet);
+%caxis([maxcolorbar-30 maxcolorbar]);
+title('Range Compressed Data');
+xlabel('Waypoints (m)');
+ylabel('Range (m)');
+ylim([0 125]);
+
+subplot(3,2,2);
+maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
+contourf(1:400,range,abs(range_compressed_matrix),'edgecolor','none');
+%contourf(20*log10(abs(range_compressed_matrix)),'edgecolor','none');
+colorbar;
+colormap(jet);
+%caxis([maxcolorbar-30 maxcolorbar]);
+title('Range Compressed Data');
+xlabel('Waypoints (m)');
+ylabel('Range (m)');
+ylim([95 125]);
+
+
+
+figure;
+%subplot(3,2,1);
+%maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
+imagesc(1:400,range,abs(range_compressed_matrix));
+colorbar;
+colormap(jet);
+%caxis([maxcolorbar-20 maxcolorbar]);
+title('Range Compressed Data');
+xlabel('Waypoints (m)');
+ylabel('Range (m)');
+
+
+subplot(3,2,1);
+%maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
+imagesc(1:400,range,abs(range_compressed_matrix));
+colorbar;
+colormap(jet);
+%caxis([maxcolorbar-20 maxcolorbar]);
+title('Range Compressed Data');
+xlabel('Waypoints (m)');
+ylabel('Range (m)');
+ylim([0 125]);
+
+subplot(3,2,2);
+%maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
+imagesc(1:400,range,abs(range_compressed_matrix));
+colorbar;
+colormap(jet);
+%caxis([maxcolorbar-20 maxcolorbar]);
+title('Range Compressed Data');
+xlabel('Waypoints (m)');
+ylabel('Range (m)');
+ylim([95 125]);
+
+%***************** Plot the maximum of range compression  ******************
+
+ 
+
+
+plot(waypoints,y_max);
+hold on
+plot(waypoints,distance_matrix);
+title('Range over aeroplane movement');
+xlabel('Waypoints (m)');
+ylabel('Range to target (m)');
+legend('Calculated Distance','Estimated Distance');
+
+
+
+%***************** Plot Range Migration with range compressed data   ******************
+
+
+
+plot(waypoints,RMC);
+hold on
+plot(waypoints,distance_matrix);
+hold off
+title('Range over aeroplane movement');
+xlabel('Waypoints (m)');
+ylabel('Range to target (m)');
+legend('Range Cell Migration correction');
+
+
+
+%***************** Plot migration correction    ******************
+
+
+
+plot(waypoints,RMC);
+hold on
+plot(RMC,azimuth);
+hold off
+title('Range over aeroplane movement');
+xlabel('Waypoints (m)');
+ylabel('Range to target (m)');
+legend('Range Cell Migration correction','Azimuth compression');
+ylim([0 98]);
+
+figure;
+maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
+contourf(1:400,range,20*log10(abs(migration_compressed_matrix)),'edgecolor','none');
+colorbar;
+colormap(jet);
+caxis([maxcolorbar-30 maxcolorbar-10]);
+title('Migration Compressed Data');
+xlabel('Waypoints (m)');
+ylabel('Range (m)');
+
+%***************** Plot azimuth correction    ******************
+
+contourf(azimuth2,range,abs(azimuth_compressed_matrix),'edgecolor','none');
+colorbar;
+colormap(jet);
+title('Azimuth Compressed Data');
+xlabel('Waypoints (m)');
+ylabel('Range (m)');
+
+figure;
+plot(waypoints,azimuth2);
+hold on
+plot(waypoints,RMC);
+plot(x,y,'^r');
+title('Range over aeroplane movement');
+xlabel('Waypoints (m)');
+ylabel('Range (m)');
+legend('Azimuth Compression','Range Migration Correction',sprintf('(%2.0f,%2.0f) Target',x,y));
+
--- MATLAB/Passive Radar Simulator/processamento_sar_bistatico.m
+++ MATLAB/Passive Radar Simulator/processamento_sar_bistatico.m
@@ -1,5 +1,5 @@
 
-% Range compression with reference signal
+%***************** Range Compression ******************
 
 % Normalized values
 maxval=max([max(max(abs(surv_matrix))),max(max(abs(ref_matrix)))]);
@@ -30,219 +30,115 @@ end
 range=time_compression_cut*c;
 
 
+%***************** Range compressed data for one target ******************
 
-% Range compressed data representation
-figure;
-maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
-contourf(1:400,range,abs(range_compressed_matrix),'edgecolor','none');
-%contourf(20*log10(abs(range_compressed_matrix)),'edgecolor','none');
-colorbar;
-colormap(jet);
-%caxis([maxcolorbar-30 maxcolorbar]);
-title('Range Compressed Data');
-xlabel('Waypoints (m)');
-ylabel('Range (m)');
-
-subplot(3,2,1);
-maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
-contourf(1:400,range,abs(range_compressed_matrix),'edgecolor','none');
-%contourf(20*log10(abs(range_compressed_matrix)),'edgecolor','none');
-colorbar;
-colormap(jet);
-%caxis([maxcolorbar-30 maxcolorbar]);
-title('Range Compressed Data');
-xlabel('Waypoints (m)');
-ylabel('Range (m)');
-ylim([0 125]);
-
-subplot(3,2,2);
-maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
-contourf(1:400,range,abs(range_compressed_matrix),'edgecolor','none');
-%contourf(20*log10(abs(range_compressed_matrix)),'edgecolor','none');
-colorbar;
-colormap(jet);
-%caxis([maxcolorbar-30 maxcolorbar]);
-title('Range Compressed Data');
-xlabel('Waypoints (m)');
-ylabel('Range (m)');
-ylim([95 125]);
-
-
-
-figure;
-%subplot(3,2,1);
-%maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
-imagesc(1:400,range,abs(range_compressed_matrix));
-colorbar;
-colormap(jet);
-%caxis([maxcolorbar-20 maxcolorbar]);
-title('Range Compressed Data');
-xlabel('Waypoints (m)');
-ylabel('Range (m)');
-
-
-subplot(3,2,1);
-%maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
-imagesc(1:400,range,abs(range_compressed_matrix));
-colorbar;
-colormap(jet);
-%caxis([maxcolorbar-20 maxcolorbar]);
-title('Range Compressed Data');
-xlabel('Waypoints (m)');
-ylabel('Range (m)');
-ylim([0 125]);
-
-subplot(3,2,2);
-%maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
-imagesc(1:400,range,abs(range_compressed_matrix));
-colorbar;
-colormap(jet);
-%caxis([maxcolorbar-20 maxcolorbar]);
-title('Range Compressed Data');
-xlabel('Waypoints (m)');
-ylabel('Range (m)');
-ylim([95 125]);
-
-
-% Identify all the maximum of range compressed
 [max_correlation, row_idx] = max(range_compressed_matrix, [], 1);
 y_max = time_compression_cut(row_idx);
 y_max = y_max*c;
-x_max = waypoints;
-
-% Plot the maximum of range compression
-
-Error=(y_max/distance_matrix)*100;
-plot(x_max,y_max);
-hold on
-plot(x_max,distance_matrix);
-title('Range over aeroplane movement');
-xlabel('Waypoints (m)');
-ylabel('Range to target (m)');
-legend('Calculated Distance','Estimated Distance');
 
 
-%RCMC
+%***************** Range migration correction ******************
 
 
 % Calculate necessary time delay between columns related to the column that has the minimum
 % distance
 
-[rows,columns]=size(distance_matrix);
-[R0,ixR0]=min(distance_matrix(distance_matrix ~= 0));
-first_idx = find(distance_matrix, 1, 'first');
-last_idx  = find(distance_matrix, 1, 'last');
-[rowOfMin, colOfMin] = find(distance_matrix == R0);  % Find row and col of min.
-tshift=zeros(rows,columns);
+
+[rows,columns]=size(y_max);
+distance_correction=zeros(rows,columns);
+for i = 1:size(y_max,1)
+thisrow=y_max(i,:);
+[R0,ixR0]=min(thisrow(thisrow ~= 0));
+first_idx = find(thisrow, 1, 'first');
+last_idx  = find(thisrow, 1, 'last');
+[rowOfMin, colOfMin] = find(thisrow == R0); % Find row and col of min.
 for col = 1 : columns
-        thisColumn = distance_matrix(:, col);
+        thisColumn = y_max(i, col);
         if thisColumn == 0
             idx2=0;
             continue
         end
         idx2=col-colOfMin;
-        if idx2 == 0
+        if idx2 == 0;
             dist=0;
             continue
         end
-        dist=thisColumn-R0;
-        tshift(:,col)=dist./c;
+        dist=(thisColumn-R0);
+        distance_correction(i,col)=dist./1;
 
 end
+end
 
-% Distance correction
+% Distance correction for each column0
 
-distance_correction = tshift*c;
-RMC=distance_matrix-distance_correction;
-expectcol=mean(first_idx:last_idx);
-expectvalue=R0;
-
-
-plot(x_max,RMC);
-hold on
-plot(x_max,y_max);
-%plot(expectcol, expectvalue, '^r');
-hold off
-title('Range over aeroplane movement');
-xlabel('Waypoints (m)');
-ylabel('Range to target (m)');
-legend('Range Cell Migration correction','Calculated Distance in Range Compression','Expected Distance to Target');
-
-isR=find(y_max);
-y_cutted=y_max(isR);
-[a,b]=intersect(range,y_cutted);
-b=b.';
-RMC2=RMC(:,184:255);
-range(:,(254:325))=RMC2;
-
-
-figure;
-%subplot(3,2,1);
-%maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
-imagesc(1:400,range,abs(range_compressed_matrix));
-colorbar;
-colormap(jet);
-%caxis([maxcolorbar-20 maxcolorbar]);
-title('Range Migrated Data');
-xlabel('Waypoints (m)');
-ylabel('Range (m)');
-
-
-subplot(3,2,1);
-%maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
-imagesc(1:400,range,abs(range_compressed_matrix));
-colorbar;
-colormap(jet);
-%caxis([maxcolorbar-20 maxcolorbar]);
-title('Range Compressed Data');
-xlabel('Waypoints (m)');
-ylabel('Range (m)');
-ylim([0 125]);
-
-subplot(3,2,2);
-%maxcolorbar=max(max(20*log10(abs(range_compressed_matrix))));
-imagesc(1:400,range,abs(range_compressed_matrix));
-colorbar;
-colormap(jet);
-%caxis([maxcolorbar-30 maxcolorbar-10]);
-title('Range Compressed Data');
-xlabel('Waypoints (m)');
-ylabel('Range (m)');
-ylim([95 125]);
-
-imagesc(azimuth,range,abs(range_compressed_matrix));
-colorbar;
-colormap(jet);
-%caxis([maxcolorbar-30 maxcolorbar-10]);
-title('Range Compressed Data');
-xlabel('Waypoints (m)');
-ylabel('Range (m)');
-
-
-% azimuth compression 
-
-
-[rows,columns]=size(range_compressed_matrix);
-for row = 1 : rows
-    thisrow = range_compressed_matrix(row, :);
-    [freq_azimuth,azimuth]=time2freq(RMC,waypoints);
+RMC=y_max-distance_correction;
+for i = 1:size(RMC,1)
+        thisrow=RMC(i,:);
+        isR=find(thisrow);
+        RMC2(i,:)=RMC(i,isR);
 end
 
 
-plot(waypoints,RMC);
-hold on
-plot(waypoints,azimuth);
-hold off
-title('Range over aeroplane movement');
-xlabel('Waypoints (m)');
-ylabel('Range to target (m)');
-legend('Range Cell Migration correction','Azimuth compression');
-ylim([0 98]);
 
-imagesc(waypoints,range,abs(range_compressed_matrix));
-colorbar;
-colormap(jet);
-%caxis([maxcolorbar-30 maxcolorbar-10]);
-title('Range Compressed Data');
-xlabel('Waypoints (m)');
-ylabel('Range (m)');
+
+%***************** Range Migration in compressed data ******************
+
+distance_correction=round(distance_correction);
+[rows2,columns2]=size(range_compressed_matrix);
+code=0;
+    for col2=1:columns2
+        thiscol=range_compressed_matrix(:,col2);
+        thiscol2=distance_correction(:,col2);
+        n=thiscol2;
+        migration=circshift(thiscol,n);
+        if n>0
+            migration(1:n) = 0;
+           else
+            migration(end+n+1:end) = 0;
+        end
+   migration_compressed_matrix(:,col2)=migration;
+   
+    end
+
+
+%%
+%***************** Azimuth compression ******************
+
+
+[rows,columns]=size(migration_compressed_matrix);
+for row = 1 :  rows
+    thisrow = migration_compressed_matrix(row, :);
+    [freq_azimuth,azimuth]=time2freq(thisrow,waypoints);
+    azimuth_compressed_matrix(row,:)=azimuth;
+end
+
+
+[rows,columns]=size(azimuth_compressed_matrix);
+for row=1:rows
+    azimuth2(row,:)=interp1(freq_azimuth,abs(azimuth),linspace(min(freq_azimuth),max(freq_azimuth),length(waypoints)));
+end
+
+
+
+
+% Expected azimuth compression
+
+[rows,columns]=size(RMC);
+for row = 1 :  rows
+    thisrow = RMC(row, :);
+    [freq_azimuth,azimuth]=time2freq(thisrow,waypoints);
+    azimuth_compressed_matrix(row,:)=azimuth.';
+end
+
+azimuth2=interp1(freq_azimuth,abs(azimuth),linspace(min(freq_azimuth),max(freq_azimuth),length(waypoints)));
+
+[min,posin] = find(RMC, 1, 'first');
+[min2,posout]  = find(RMC, 1, 'last');
+meanRMC=(posout+posin)/2;
+[val,pos] = max(azimuth2);
+shift=abs(meanRMC-pos);
+azimuth2=circshift(azimuth2,shift);
+
+[M,I]=max(azimuth2);
+y=RMC(I);
+x=I;
