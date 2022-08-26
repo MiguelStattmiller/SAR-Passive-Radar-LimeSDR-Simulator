836d07b18bec28d1bed1c541cdbd0cac086fa672
MiguelStattmiller <80427307+MiguelStattmiller@users.noreply.github.com>
2022-08-26 12:39:06 +0100

    Update Colin_vectors.m

--- MATLAB/Library/Colin_vectors.m
+++ MATLAB/Library/Colin_vectors.m
@@ -1,3 +1,5 @@
+% This function allows to identify colinearity between two vectors
+
 function [Vectors_product_2] = Colin_vectors(VTransmitter_target,VTarget_receiver)
 
 Vectors_product_2=dot(VTransmitter_target,VTarget_receiver)/(norm(VTransmitter_target)*norm(VTarget_receiver));
